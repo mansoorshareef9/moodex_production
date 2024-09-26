10c9207c940e9d1e49b8c9439dd190452da6c5c3
code_zombiee <mansoorshareef9@gmail.com>
2024-09-26 12:19:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,25 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '640px',  // Small devices (mobile)
+      md: '768px',  // Medium devices (tablets)
+      lg: '1024px', // Large devices (laptops)
+      xl: '1280px', // Extra large devices (desktops)
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',  // Default for mobile
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem',
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
